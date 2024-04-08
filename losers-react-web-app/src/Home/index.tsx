36a28f939aebe24b187b0e32b5ef766ab46b38c1
nhzq-1010 <18802121321@163.com>
2024-04-08 18:00:20 -0400

    111

--- losers-react-web-app/src/Home/index.tsx
+++ losers-react-web-app/src/Home/index.tsx
@@ -1,9 +1,11 @@
 import Nav from "../Nav";
+import NavTopBar from "./navbar";
 
 function HomePage() {
 
   return (
     <>
+    <NavTopBar />
       <Nav />
       <h1>This will be our future home page</h1>
 
