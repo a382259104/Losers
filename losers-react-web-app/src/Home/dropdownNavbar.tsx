5c9dd1ced84cb300a1a5f26903d2071fa59bb21d
nhzq-1010 <18802121321@163.com>
2024-04-08 19:21:22 -0400

    icon

--- losers-react-web-app/src/Home/dropdownNavbar.tsx
+++ losers-react-web-app/src/Home/dropdownNavbar.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import"./index.css";
+import "./index.css";
+import { Link } from 'react-router-dom';
+import {FaRegUserCircle } from "react-icons/fa";
+
 
 function DropdownNavbar() {
   const [isNavCollapsed, setIsNavCollapsed] = useState(true);
@@ -8,10 +11,17 @@ function DropdownNavbar() {
 
   return (
     <nav className=" navbar navbar-expand-lg navbar-dark bg-dark" >
-      <a className="nav-layout navbar-brand" href="#">Navbar</a>
-      <button className="nav-layout navbar-toggler" type="button" onClick={handleNavCollapse}>
-        <span className="navbar-toggler-icon"></span>
-      </button>
+      <span>
+        <a className="nav-layout navbar-brand" href="#">Navbar</a>
+        <button className="nav-layout navbar-toggler" type="button" onClick={handleNavCollapse}>
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+      </span>
+      <Link to={""} className="icon navbar-brand float-end" >
+        <FaRegUserCircle className="fs-3" /> </Link>
+
+
       <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`}>
         <ul className="navbar-nav mr-auto">
           <li className="nav-dropdown-layout nav-item">
@@ -25,7 +35,7 @@ function DropdownNavbar() {
           </li>
         </ul>
         <form role="search">
-          <input className="form-control" type="search" placeholder="Search" aria-label="Search"/>
+          <input className="form-control" type="search" placeholder="Search" aria-label="Search" />
         </form>
       </div>
     </nav>
