78f2531eec19912393f8154b224e8a95797e5e7d
YuFeng Lin <a382259104@gmail.com>
2024-04-10 15:25:41 -0400

    small changes

--- losers-sever/App.js
+++ losers-sever/App.js
@@ -8,6 +8,22 @@ const CONNECTION_STRING = `mongodb+srv://a382259104:zXu2y332Fxc9olNj@winners.uad
 
 mongoose.connect(CONNECTION_STRING);
 
+// Event listener for successful connection
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose connected to MongoDB');
+  });
+  
+  // Event listener for connection error
+  mongoose.connection.on('error', (err) => {
+    console.error('Mongoose connection error:', err);
+  });
+  
+  // Event listener for disconnected
+  mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected');
+  });
+
+
 UserRoutes(app)
 
 app.listen(process.env.PORT);
